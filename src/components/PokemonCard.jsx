822ebcd533fa971c3eb51bfd55187fe54bdd8fd3
Victor G <victorgj95@outlook.com>
2023-10-08 18:49:10 +0200

    React 1st challenge done

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,29 @@
+// tableau pokemonList
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+//fonction pokemonCard()
 function PokemonCard() {
+//variable contenant le pokemon à l'index 0 du tableau pokemonList, peut être 0 ou 1
+  const pokemon = pokemonList[0];
+//la fonction renvoie ()
   return (
     <div>
       <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur"/>
+        {pokemon.imgSrc ? ( //si pokemon.imgSrc existe
+          <img src={pokemon.imgSrc} alt={pokemon.name}/> //on renvoie une image, avec pour source l'imgSrc à l'index 0 de pokemon + alt
+        ) : ( // sinon
+          <p>???</p> //on renvoie ??? dans une balise p
+        )}
       </figure>
-      <figcaption>Bulbasaur</figcaption>
+      <figcaption>{pokemon.name}</figcaption> {/* pour afficher le nom du pokemon */}
     </div>
   );
 }
